dfd457f06ee6ba84a802685313228292823485d8
PlohejPL <s13384@pjwstk.edu.pl>
2019-12-07 20:31:29 +0100

    Set dev mode to true

--- config/defines.inc.php
+++ config/defines.inc.php
@@ -30,7 +30,7 @@ define('_PS_MODE_DEV_', true);
 }
 /* Compatibility warning */
 define('_PS_DISPLAY_COMPATIBILITY_WARNING_', false);
-if (_PS_MODE_DEV_ === true) {
+if (/*_PS_MODE_DEV_*/ true === true) {
     @ini_set('display_errors', 'on');
     @error_reporting(E_ALL | E_STRICT);
     define('_PS_DEBUG_SQL_', true);
